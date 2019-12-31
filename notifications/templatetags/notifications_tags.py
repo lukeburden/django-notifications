2807ae5657386fd96dad9cb0c2d3234d33917966
Luke Burden <lukeburden@gmail.com>
2019-12-31 13:53:52 +1100

    Fix use of user.is_anonymous

--- notifications/templatetags/notifications_tags.py
+++ notifications/templatetags/notifications_tags.py
@@ -67,6 +67,6 @@ def user_context(context):
 
     request = context['request']
     user = request.user
-    if user.is_anonymous():
+    if user.is_anonymous:
         return None
     return user
